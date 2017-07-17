b3e7ece2de025d2c1daba8814d95fe6dc884787c
rohitgpawar <rohitpawar.social@gmail.com>
2017-07-17 16:04:36 -0500

    CCI Palindrome Permutation

--- ProblemSolving/Arrays and Strings/PalindromePermutation.cs
+++ ProblemSolving/Arrays and Strings/PalindromePermutation.cs
@@ -1,13 +1,16 @@
 ﻿/*
- Check if a string is permutation of a palindrome.
+Cracking Coding Interviews. Array and Strings. 
+Check if a string is permutation of a palindrome.
  */
 namespace ProblemSolving
 {
     public class PalindromePermutation
     {
+        /*Count How many characters have odd count in the string. For that use bool array to toggle count of odd charachters. 
+        check the odd count at the end if odd count is more than one then string permutation not palindrom*/
         public static bool CheckPalindromePermutation(string inputString)
-        {
-            bool[] isOddCharachter = new bool[128];
+        {//Bool array to store the isOdd value of each character in the string. Convert every character to lower case so we need only 128 characters to track.
+            bool[] isOddCharacter = new bool[128];
             int oddCharCount=0;
             foreach (char chr in inputString.ToLower())
             {
@@ -15,17 +18,17 @@ namespace ProblemSolving
                 {
                     continue;
                 }
-                isOddCharachter[chr] = !isOddCharachter[chr];
-                if (isOddCharachter[chr])
-                {
+                isOddCharacter[chr] = !isOddCharacter[chr];//Toggle isOddCharacter value.
+                if (isOddCharacter[chr])
+                {//isOddCharacter is true means count is odd.
                     oddCharCount++;
                 }
                 else
-                {
+                {//isOddCharacter is false means count is even.
                     oddCharCount--;
                 }
             }
-            if (oddCharCount < 2)
+            if (oddCharCount < 2) //Id odd count is more than 1 then its not palindrome.
                 return true;
             return false;
         }
