6b5e5f9c6694a837e68128c91e8e2b06d2d98b3b
rohitgpawar <rohitpawar.social@gmail.com>
2018-02-11 00:00:01 -0500

    Amazon Interview Prep

--- ProblemSolving/Company/Amazon/ConvertBinaryTreeToSumTree.cs
+++ ProblemSolving/Company/Amazon/ConvertBinaryTreeToSumTree.cs
@@ -1,20 +1,21 @@
 ﻿/*
  Convert a given tree to its Sum Tree
 2.6
-Given a Binary Tree where each node has positive and negative values. Convert this to a tree where each node contains the sum of the left and right sub trees in the original tree. The values of leaf nodes are changed to 0.
+Given a Binary Tree where each node has positive and negative values. Convert this to a tree where each 
+node contains the sum of the left and right sub trees in the original tree. The values of leaf nodes are changed to 0.
 
 For example, the following tree
 
                   10
                /      \
-         -2        6
+            -2        6
            /   \      /  \ 
          8     -4    7    5
 should be changed to
 
-                 20(4-2+12+6)
+              20(4-2+12+6)
                /      \
-       4(8-4)      12(7+5)
+            4(8-4)      12(7+5)
            /   \      /  \ 
          0      0    0    0
  
